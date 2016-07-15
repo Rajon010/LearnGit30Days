e62dd1b92dbdc5e34f7930c2545a8b22b3d9441e
Rajon <rajon@uwan.com.tw>
2016-07-15 15:50:45 +0800

    modify Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,7 @@ public class Main {
 		queue.enqueue(11);
 		queue.enqueue(12);
 		queue.enqueue(13);
-		queue.enqueue(0);
-		queue.dequeue();
-		queue.enqueue(1);
-		queue.dequeue();
-		queue.enqueue(2);
-		queue.dequeue();
-		queue.dequeue();
-		queue.enqueue(3);
-		queue.enqueue(4);
-		queue.enqueue(5);
-		queue.dequeue();
-		queue.dequeue();
+RRRRRRRRR
 		queue.dequeue();
 		queue.dequeue();
 	}
